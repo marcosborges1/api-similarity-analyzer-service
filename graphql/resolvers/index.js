b84605e9bdf3c470f01ff5d8b13fe0c2e3884e5c
Marcos Vinicius Borges <marcosviniciusborges@Marcoss-MacBook-Air.local>
2022-02-18 01:48:23 -0300

    Version ok, similarities

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -38,6 +38,8 @@ const resolvers = {
 					api["requests"] = [reportRequestsAPI];
 					api["responses"] = [reportResponsesAPI];
 				});
+				requests.push([reportRequestsAPI]);
+				responses.push([reportResponsesAPI]);
 			});
 			// Time to ensure that requests and responses are integrated for each API
 			await wait(200);
@@ -89,67 +91,17 @@ const resolvers = {
 					});
 				});
 			});
-			console.log(similarities);
 
 			return {
-				requests: null,
-				responses: null,
-				similarities,
+				requests: requests,
+				responses: responses,
+				similarities: similarities,
 			};
 		},
 	},
 };
 
 module.exports = resolvers;
-
-// dataAPI.map(async (origin, i) => {
-// 	await dataAPI.slice(i + 1).map(async (target) => {
-// 		let reportRequestsAPI = {};
-// 		let reportResponsesAPI = {};
-// 		const parser = new SwaggerParser();
-// 		const originRef = await parser.dereference(origin.path);
-// 		const originKeys = Object.keys(originRef.paths);
-// 		const originValues = Object.values(originRef.paths);
-
-// 		originValues.map(async (value, i) => {
-// 			reportResponsesAPI[originKeys[i]] = "";
-// 			["get", "post"].map((method) => {
-// 				getResponsesApi(
-// 					origin.name,
-// 					value,
-// 					method,
-// 					reportResponsesAPI,
-// 					originKeys,
-// 					i
-// 				);
-// 				getRequestsAPI(value, method, reportRequestsAPI, originKeys, i);
-// 			});
-// 		});
-// 		origin["requests"] = reportRequestsAPI;
-// 		origin["responses"] = reportResponsesAPI;
-
-// 		// responses.push(reportResponsesAPI);
-
-// 		reportRequestsAPI = {};
-// 		reportResponsesAPI = {};
-// 		const targetRef = await parser.dereference(target.path);
-// 		const targetKeys = Object.keys(targetRef.paths);
-// 		const targetValues = Object.values(targetRef.paths);
-// 		targetValues.map(async (value, i) => {
-// 			reportRequestsAPI[targetKeys[i]] = "";
-// 			["get", "post"].map(async (method) => {
-// 				getRequestsAPI(value, method, reportRequestsAPI, targetKeys, i);
-// 			});
-// 		});
-
-// 		// requests.push(reportRequestsAPI);
-
-// 		// console.log(reportResponsesAPI, reportRequestsAPI);
-// 	});
-// });
-// await wait(200);
-
-// console.log(dataAPI);
 
 // const apis = [
 // 	{
@@ -179,178 +131,3 @@ module.exports = resolvers;
 // 		],
 // 	},
 // ];
-// const similarities = [];
-// apis.map((currentApi, i) => {
-// 	apis.slice(i + 1).map((targetApi) => {
-// 		currentApi.responses.map((response, itRes) => {
-// 			targetApi.requests.map((request, itReq) => {
-// 				Object.values(Object.values(response)[0])[0].map((res) => {
-// 					Object.values(Object.values(request)[0])[0].map((req) => {
-// 						if (Object.keys(res)[0] == Object.keys(req)[0]) {
-// 							let origin = {};
-// 							origin["api"] = currentApi.name;
-// 							origin["url"] = Object.keys(response)[0];
-// 							origin["method"] = Object.keys(
-// 								Object.values(response)[0]
-// 							)[0];
-// 							origin["parametersIn"] = Object.values(
-// 								Object.values(currentApi.requests[itRes])[0]
-// 							)[0];
-// 							origin["parametersOut"] = Object.values(
-// 								Object.values(currentApi.responses[itRes])[0]
-// 							)[0];
-// 							let target = {};
-// 							target["api"] = targetApi.name;
-// 							target["url"] = Object.keys(request)[0];
-// 							target["method"] = Object.keys(
-// 								Object.values(request)[0]
-// 							)[0];
-// 							target["parametersIn"] = Object.values(
-// 								Object.values(targetApi.requests[itReq])[0]
-// 							)[0];
-// 							target["parametersOut"] = Object.values(
-// 								Object.values(targetApi.responses[itReq])[0]
-// 							)[0];
-// 							similarities.push({ originAPI: origin, targetAPI: target });
-// 						}
-// 					});
-// 				});
-// 			});
-// 		});
-// 	});
-// });
-
-// const parametersOutByAPI = dataAPI.map(async (api, i) => {
-// 	const parser = new SwaggerParser();
-// 	const currentAPI = await parser.dereference(api.path);
-// 	const currentAPIKeys = Object.keys(currentAPI.paths);
-// 	const currentAPIValues = Object.values(currentAPI.paths);
-// 	let reportparametersInAPI = [];
-// 	currentAPIValues.map(async (apiValue, j) => {
-// 		["get", "post"].map((method) =>
-// 			getRequestsAPI(apiValue, reportparametersInAPI, currentAPIKeys, i)
-// 		);
-// 	});
-// 	// console.log(reportparametersInAPI.map((r) => r));
-
-// 	// console.log(currentAPIValues.map((val) => Object.keys(val)));
-// 	// const x = {};
-// 	// x["reponse"] = reportparametersOutByAPI;
-// 	// x[api.name] = x["reponse"];
-// 	// console.log(x);
-
-// 	// console.log(Object.values(reportparametersInAPI)[0]);
-// });
-
-// const similaritiesIda = [];
-
-// const similarities2 = await dataAPI.map(async (origin, i) => {
-// 	await dataAPI.slice(i + 1).map(async (target) => {
-// 		let reportResponsesAPI = {};
-
-// 		const parser = new SwaggerParser();
-// 		const originRef = await parser.dereference(origin.path);
-// 		const originKeys = Object.keys(originRef.paths);
-// 		const originValues = Object.values(originRef.paths);
-
-// 		originValues.map(async (value, i) => {
-// 			reportResponsesAPI[originKeys[i]] = "";
-// 			["get", "post"].map((method) => {
-// 				getResponsesApi(
-// 					origin.name,
-// 					value,
-// 					method,
-// 					reportResponsesAPI,
-// 					originKeys,
-// 					i
-// 				);
-// 			});
-// 		});
-
-// 		let reportRequestsAPI = {};
-// 		const targetRef = await parser.dereference(target.path);
-// 		const targetKeys = Object.keys(targetRef.paths);
-// 		const targetValues = Object.values(targetRef.paths);
-// 		targetValues.map(async (value, i) => {
-// 			reportRequestsAPI[targetKeys[i]] = "";
-// 			["get", "post"].map(async (method) => {
-// 				getRequestsAPI(value, method, reportRequestsAPI, targetKeys, i);
-// 			});
-// 		});
-// 		similaritiesIda.push(reportResponsesAPI);
-// 		// console.log(reportResponsesAPI, reportRequestsAPI);
-// 	});
-// });
-// await wait(200);
-// console.log(similaritiesIda);
-
-// 		// let targetRef = await parser.dereference(target.path);
-// 		// const targetKeys = Object.keys(targetRef.paths);
-// 		// const targetValues = Object.values(targetRef.paths);
-
-// 		// console.log(targetValues);
-// 		// return origin.name + " " + target.name;
-// 	});
-// });
-// .splice(0, newDataAPI.length - 1);
-// const reverse = newDataAPI.reverse();
-// console.log(similarities);
-
-//Get Request and Response on the first time
-// let reportAPItotal = [];
-// newDataAPI.map(async (api, k) => {
-// 	let parser = new SwaggerParser();
-// 	let ref = await parser.dereference(api.path);
-// 	const keys = Object.keys(ref.paths);
-// 	const values = Object.values(ref.paths);
-// 	let reportAPI = {};
-// 	let keyMethod = {};
-// 	values.map(async (value, i) => {
-// 		keyMethod = {};
-// 		reportAPI[keys[i]] = "";
-
-// 		["get", "post"].map((method) =>
-// 			getRequestsApi(method, value, keyMethod, reportAPI, keys, i)
-// 		);
-// 	});
-// 	newDataAPI[k].res = reportAPI;
-// 	// reportAPItotal.push("marcos");
-// 	console.log(newDataAPI[k]);
-// });
-// console.log(reportAPItotal);
-
-// let parser = new SwaggerParser();
-
-// // let api = await parser.dereference("/Users/marcosviniciusborges/Documents/MyApplications/micrographql/openapi/events-api-from-web.json");
-// let api = await parser.dereference(
-// 	"/Users/marcosviniciusborges/Documents/MyApplications/micrographql/openapi/events-api-from-web.json"
-// );
-
-// const keys = Object.keys(api.paths);
-// const values = Object.values(api.paths);
-
-// let reportAPI = {};
-// let keyMethod = {};
-
-// values.map((value, i) => {
-// 	keyMethod = {};
-// 	reportAPI[keys[i]] = "";
-
-// 	["get", "post"].map((method) =>
-// 		getRequestsApi(method, value, keyMethod, reportAPI, keys, i)
-// 	);
-// });
-// console.log(reportAPI);
-// return {
-// 	requests: reportAPI,
-// 	responses: reportAPI,
-// 	similarities: [
-// 		{
-// 			originAPI: {
-// 				name: "APITechC",
-// 				method: "get",
-// 				inputParameters: [{ buid: "int", customerID: "int" }],
-// 			},
-// 		},
-// 	],
-// };
